6aecad1101ec861b896248fa5062ec09b6ecc7af
areebahmed65 <89004554+areebahmed65@users.noreply.github.com>
2022-01-08 02:10:38 +0500

    Add files via upload

---
+++ Project/Naive_Bayes.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+# READING CSV FILE
+train_data = pd.read_csv('/train.csv')
+test_data = pd.read_csv('/test.csv')
+
+test_data.head(2)
+
+train_data.head(2)
+
+train = pd.DataFrame(train_data)
+# SPLITTING AND DISTRIBUTING DATA ON X AND Y AXIS
+X = train.Elevation
+Y = train.Cover_Type
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2,random_state=109)
+print("X_train : ",len(X_train))
+print("X_test : ",len(X_test))
+print("X_train : ",len(y_train))
+print("X_test : ",len(y_test))
+
+# TRAINGING MODEL
+model =MultinomialNB()
+scaler = MinMaxScaler()
+A= X_train.values.reshape(-1,1)
+X_train = scaler.fit_transform(A)
+Test = test_data.values.reshape(-1,1)
+X_test = scaler.fit_transform(Test)
+model.fit(X_train,y_train)
+L = model.predict(Test)
+# Model (NAIYE_BAYES) Accuracy
+print("Accuracy:",metrics.accuracy_score(Test, L))
+
+id = test_data["Id"]
+Test = pd.DataFrame(id)
+arr=[]
+for row in id:
+  arr.append(L[row])
+Test["Cover type"] = arr
+Test.to_csv('SCORE.csv',index = False)
